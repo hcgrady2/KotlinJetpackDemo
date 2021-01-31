8c22fae45b321c60a5c37f660aad47094fac2788
hcc007 <hcwang1024@163.com>
2021-01-31 15:46:11 +0800

    init

---
+++ app/src/main/java/com/hc/kotlinjetpackdemo/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.hc.kotlinjetpackdemo.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
